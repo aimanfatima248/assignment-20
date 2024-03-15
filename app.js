ddf50a34189b057b66a33cd7afa5ef068e306a35
aimanfatima248 <aimanfatima65@outlook.com>
2024-03-16 01:22:36 +0500

    add ass 20

---
+++ app.js
@@ -0,0 +1,150 @@
+// CHAP 17-20
+// QS1
+// var multiarr=[[[[]]]];
+// console.log(multiarr)
+
+// QS2
+// var multi = [
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1]
+// ];
+
+// console.log(multi )
+
+// QS3
+// for (var i = 1; i <=10; i++) {
+//     document.write(i + "<br>");
+//   }
+// QS4
+// var userInput = +prompt("Enter a number to show its multiplication table");      
+
+// var tableRange = +prompt("Enter length multiplication table");   
+    
+// for (var i = 1; i <= tableRange; i++) {   
+//   document.write(userInput + "x" + i + "=" + userInput * i + "<br>");            
+// }
+
+// QS5
+// var fruits=["apple","banana","mango","orange","strawberry"];
+
+// for (var i=0 ; i<fruits.length; i++){
+//     document.write(fruits[i] + "<br>")}
+
+// for (var i=0 ; i<fruits.length; i++){
+    
+//      document.write("element at index " + i + " is " +  fruits[i] + "<br>")
+     
+// }
+
+// Q6
+// a
+// document.write("Counting" + "<br>" )
+
+// for (var i=1; i<=15;i++) {
+//     document.write(i + "<br>")
+// }; 
+
+// b
+// document.write(" Reverse Counting" + "<br>" )
+
+// for (var i=10; i>=1;i--) {
+//     document.write(i + "<br>")
+// }; 
+// c
+// var num = 20; 
+// for (var i = 0; i <= num; i += 2) {
+//     document.write(i+ "<br>");
+// }
+
+// d
+// var num = 19; 
+// for (var i = 1; i <= num; i += 2) {
+//     document.write(i+ "<br>");
+// }
+
+// e
+// for (var i = 1; i <= 10; i++) {
+//     document.write(2 * i + "k"+ "<br>");
+// }
+
+
+// QS8
+// var A = [24, 53, 78, 91, 12];
+
+// for (var i=0; i<A.length;i++)
+// document.write( "array items"+ A[i] + "<br>");
+
+
+// var largest = A[0];
+
+// for (var i = 1; i < A.length; i++) {
+//     if (A[i] > largest) {
+//         largest = A[i];
+//     }
+// }
+
+// document.write("The largest number in the array is:", largest);
+
+// Q9
+// var A = [24, 53, 78, 91, 12];
+
+// for (var i=0; i<A.length;i++)
+// document.write( "array items"+ A[i] + "<br>");
+
+
+// var smallest = A[0];
+
+// for (var i = 1; i < A.length; i++) {
+//     if (A[i] < smallest) {
+//         smallest = A[i];
+//     }
+// }
+
+// document.write("The smallest number in the array is:", smallest);
+
+// Q10
+// var num =100
+// for(var i=5; i<=num;i+=5){
+//     document.write([i] + "<br>")
+// }
+
+
+// chap 17-20
+// qs1
+// for (var i = 1; i <= 10; i++) {
+//     
+//     console.log( i);
+// }
+// qs2
+// for (var i = 1; i <= 12; i++) {
+    
+//     console.log( i);
+// }
+// qs3
+// for (var i = 0; i<=4; i++){
+
+// }
+// qs4
+// var j=100;
+// for (var j=1; j<=100;j++){
+//     console.log(j);
+// }
+// qs5
+// var i=3;
+// for(i=3;i>0;i--){
+//     console.log(i)
+// }
+// qs6
+// var array=["a",2,7,"m",8,6];
+// console.log(array)
+// qs7
+// var flag=true;
+// console.log(flag)
+// qs8
+// var pets=["birds","dogs","cats","goat"]
+// var length=pets.length;
+// for(var i=0;i<length;i++){
+//     console.log(pets[i])
+// }
+
